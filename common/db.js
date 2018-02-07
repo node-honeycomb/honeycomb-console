ea1d146e35ae360cc355d03570a4836b4e0f1d88
huangyao <yusong.hy@alibaba-inc.com>
2018-02-07 10:46:55 +0800

    fix url

--- common/db.js
+++ common/db.js
@@ -2,8 +2,6 @@ const config = require('../config');
 const meta = config.meta;
 switch (meta.driver) {
   case 'mysql':
-    module.exports = require('./sequelize');
-    break;
   case 'sqlite':
     module.exports = require('./sequelize');
     break;
