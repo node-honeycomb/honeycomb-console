080e0a37da59fd197337137c69582fee9b18c89f
kangyu <rogerkang19@gmail.com>
2018-02-12 11:22:55 +0800

    configurable publish page

--- controller/page.js
+++ controller/page.js
@@ -35,7 +35,7 @@ exports.pages = function (req, callback) {
       let e = new Error('Get cluster config from db failed.' + err.message);
       return callback(e);
     }
-
+    console.log('config.publishPages', JSON.stringify(config.publishPages));
     callback(null, {
       tpl: 'index.html',
       data: {
@@ -43,7 +43,7 @@ exports.pages = function (req, callback) {
         csrfToken: req.csrfToken(),
         user: req.user,
         env: config.env,
-        publishPage: config.publishPage
+        publishPages: Array.isArray(config.publishPages) ? config.publishPages : []
       }
     }, 'html');
   });
