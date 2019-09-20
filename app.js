fceec1d7456dbe1706f4a811e9f6e497f9d8d040
jianxun.zxl <jianxun.zxl@taobao.com>
2019-09-20 11:20:25 +0800

    feat: adjust max upload for large pkg

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ const App = require('hc-bee');
 const app = new App();
 const config = require('./config');
 
+app.server.setTimeout(300000);
+
 config.username = app.config.username;
 config.password = app.config.password;
+
 app.ready(true);
+
 
 module.exports = app;
