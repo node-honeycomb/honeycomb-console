50b71fef33971f83c572581eacd41582772c8a16
jianxun.zxl <jianxun.zxl@taobao.com>
2020-09-10 11:15:19 +0800

    fix: app_package storage api

--- model/app_package.js
+++ model/app_package.js
@@ -29,9 +29,11 @@ exports.savePackage = (data, callback) => {
     return callback(null);
   }
   if (storage) {
-    storage.save(data.pkg, (err, fkey) => {
+    storage.save(data.clusterCode + '/' + data.appId + '.tgz', data.pkg, (err, fkey) => {
       save('key', fkey, callback);
     });
+  } else if (storage === false) {
+    save('key', '', callback);
   } else {
     save('file', data.pkg, callback);
   }
@@ -58,7 +60,27 @@ exports.savePackage = (data, callback) => {
  */
 const DELETE_APP_PKG = `delete from hc_console_system_cluster_app_pkgs where cluster_code = ? and app_id = ?`;
 exports.deletePackage = (clusterCode, appId, callback) => {
-  db.query(DELETE_APP_PKG, [clusterCode, appId], callback);
+  db.query(
+    GET_APP_PKG,
+    [clusterCode, appId],
+    function (err, data) {
+      if (err) {
+        log.error('delete app pkg: query pkg info failed:', err);
+        return callback(err);
+      } else {
+        log.info('delete app pkg: query pkg info successfully', JSON.stringify(data[0]));
+        if (data[0]) {
+          if (storage && storage.delete) {
+            storage.delete(data[0].package, () => {
+              db.query(DELETE_APP_PKG, [clusterCode, appId], callback);
+            });
+          } else {
+            db.query(DELETE_APP_PKG, [clusterCode, appId], callback);
+          }
+        }
+      }
+    }
+  );
 };
 
 /**
@@ -81,10 +103,10 @@ exports.getPackage = (clusterCode, appId, callback) => {
     [clusterCode, appId],
     function (err, data) {
       if (err) {
-        log.error('get app pkg failed:', err);
+        log.error('get app pkg:  failed when query pkg info:', err);
         return callback(err);
       } else {
-        log.info('get app pkg success', JSON.stringify(data[0]));
+        log.info('get app pkg: query pkg info success', JSON.stringify(data[0]));
         if (data[0]) {
           let tmpFile = path.join(os.tmpdir(), data[0].clusterCode + '^' + data[0].appId + '.tgz');
           if (storage) {
@@ -95,6 +117,8 @@ exports.getPackage = (clusterCode, appId, callback) => {
               data[0].package = tmpFile;
               callback(null, data[0]);
             });
+          } else if (storage === false) {
+            callback(null, '');
           } else {
             fs.writeFileSync(tmpFile, data[0].package);
             data[0].package = tmpFile;
