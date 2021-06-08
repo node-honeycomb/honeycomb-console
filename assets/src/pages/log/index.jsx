690ef464d47a3e70417562287ed7e2012910dc42
sdjalft <sdjalft@126.com>
2021-06-08 17:56:00 +0800

    feat: 为应用配置页、日志查询页添加了搜索栏. ref #64

--- assets/src/pages/log/index.jsx
+++ assets/src/pages/log/index.jsx
@@ -10,6 +10,7 @@ import s2q from '@lib/search-to-query';
 import msgParser from '@lib/msg-parser';
 import filepaths2tree from '@lib/filepath-to-tree';
 
+import SearchTree from './coms/search-tree';
 import FileTree from './coms/file-tree';
 import LogPanel from '../../components/log-panel';
 
@@ -24,6 +25,14 @@ const Log = (props) => {
   const [tree, setTree] = useState([]);
   const [activeLog, setActiveLog] = useState(DEFAULT_ACTIVE_LOG);
   const [initing, setIniting] = useState(true);
+  /**
+   * 搜索框的各个状态，分别是列表树的元素、上一次搜索的字符串、
+   * 用户输入的字符串和选中次数（用来通知外层的树改变形态）
+   */
+  const [fileSearchItem, setFileSearchItem] = useState([]);
+  const [lastSearchString, setLastSearchString] = useState('');
+  const [searchInputString, setSearchInputString] = useState('');
+  const [searchTimes, setSearchTimes] = useState(0);
 
   // 获取日志列表
   const getLogFiles = async (currentClusterCode) => {
@@ -90,14 +99,124 @@ const Log = (props) => {
 
   const onSelectFile = (filepath) => {
     setQuery(props.location, props.dispatch, {logFilepath: filepath});
-
     setActiveLog(filepath);
+  };
+
+  const onSelectSearchTreeItem = (filepath) => {
+    onSelectFile(filepath);
+    setSearchTimes(searchTimes + 1);
+    setFileSearchResultList('');
+  };
+
+  // 文件搜索框的下拉列表
+  const FileSearchResultList = () => {
+    return (
+      <div className="file-search-result-list">
+        {
+          fileSearchItem.length !== 0 ?
+            <SearchTree
+              tree={fileSearchItem}
+              loading={filesLoading}
+              onSelect={onSelectSearchTreeItem}
+              keywords={lastSearchString}
+            /> :
+            lastSearchString === '' ?
+              null : <div className="file-search-result-item">没有数据</div>
+        }
+      </div>
+    );
+  };
+
+  // 用户输入改变时，改变搜索到的数据
+  const setFileSearchResultList = (v) => {
+    if (v === '') {
+      setFileSearchItem([]);
+      setLastSearchString('');
+      setSearchInputString('');
+
+      return null;
+    }
+    if (v.target.value === '') {
+      setFileSearchItem([]);
+      setLastSearchString(v.target.value);
+      setSearchInputString('');
+
+      return null;
+    }
+    setSearchInputString(v.target.value);
+    // 搜寻到的item列表原型
+    let tmpSearchItemList = [];
+
+    if (v.target.value.includes(lastSearchString) && lastSearchString !== '') {
+      tmpSearchItemList = fileSearchItem.filter((item) => {
+        return item.title.includes(v.target.value);
+      });
+    } else {
+      tmpSearchItemList = tree.filter((item) => {
+        return item.title.includes(v.target.value);
+      });
+    }
+
+    // 用来去重的set
+    const tmpSet = new Set();
+    // 最终将要setstate的列表
+    const finalSearchItemList = [];
+
+    // 先将每一个元素全部都正确地插进数组中，最后再进行排序处理
+    for (let cc = 0; cc < tmpSearchItemList.length; cc++) {
+      let nowNode = tmpSearchItemList[cc];
+
+      while (nowNode) {
+        if (tmpSet.has(nowNode.key)) {
+          break;
+        }
+        finalSearchItemList.push(nowNode);
+        tmpSet.add(nowNode.key);
+        nowNode = tree.find((item) => {
+          return item.key === nowNode.parent;
+        });
+      }
+    }
+
+    setFileSearchItem(finalSearchItemList.sort((a, b) => {
+      // 由于根目录文件的特殊路径，这里需要特殊处理
+      if (a.key === '系统日志') {
+        return false;
+      }
+      if (b.key === '系统日志') {
+        return true;
+      }
+      if (a.parent === '系统日志' && b.parent !== '系统日志') {
+        return false;
+      }
+      if (b.parent === '系统日志' && a.parent !== '系统日志') {
+        return true;
+      }
+      if (a.parent === '系统日志' && b.parent === '系统日志') {
+        return a.key > b.key;
+      }
+
+      return a.key > b.key;
+    }));
+    setLastSearchString(v.target.value);
   };
 
   return (
     <div>
       <div className="page-left-side">
         <div className="list-title">日志列表</div>
+        {
+          !initing &&
+            <div>
+              <input
+                value={searchInputString}
+                className="file-search-bar"
+                placeholder="文件搜索"
+                onChange={(e) => setFileSearchResultList(e)}
+              />
+              <FileSearchResultList></FileSearchResultList>
+            </div>
+        }
         {
           !initing && (
             <FileTree
@@ -106,6 +225,7 @@ const Log = (props) => {
               onSelect={onSelectFile}
               activeKey={activeLog}
               defaultActiveKey={activeLog}
+              treeStatusChange={searchTimes}
             />
           )
         }
