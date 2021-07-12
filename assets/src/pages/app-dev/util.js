656f20e175d4b85f82cdb2c1afd90f90310d9404
wb-yf765537 <wb-yf765537@alibaba-inc.com>
2021-07-12 20:16:46 +0800

    fix: empty-fix

--- assets/src/pages/app-dev/util.js
+++ assets/src/pages/app-dev/util.js
@@ -3,6 +3,67 @@ import moment from 'moment';
 import {APP_ID_MATCH} from '@lib/consts';
 
 const TRANS = 1024 * 1024 * 1024;
+const keepOnlineNum = _.get(window, ['appManageConfig', 'keepOnlineNum']) || 3; // 保留的在线版本数量
+const keepOfflineNum = _.get(window, ['appManageConfig', 'keepOfflineNum']) || 5; // 保留的离线版本数量
+
+export const setClearPolicy = (data) => {
+  _.map(data, (value) => {
+    // eslint-disable-next-line array-callback-return
+    const onlineList = value.filter((item) => {
+      if (_.get(item, 'cluster[0].status') === 'online') {
+        return item;
+      }
+    });
+    // eslint-disable-next-line array-callback-return
+    const offlineList = value.filter((item) => {
+      if (_.get(item, 'cluster[0].status') === 'offline') return item;
+    });
+
+    let keepOnlineIdx = onlineList.length - keepOnlineNum;
+    let keepOfflineIdx = offlineList.length - keepOfflineNum;
+
+    // 在线版本数未达到上限则全部保留
+    if (keepOnlineIdx < 0) {
+      keepOnlineIdx = 0;
+    }
+    onlineList.slice(keepOnlineIdx).map(d => {
+      d.isKeepOnline = true;
+
+      return d;
+    });
+    if (keepOfflineIdx < 0) {
+      keepOfflineIdx = 0;
+    }
+    offlineList.slice(keepOfflineIdx).map(d => {
+      d.isKeepOffline = true;
+
+      return d;
+    });
+  });
+
+  return data;
+};
+
+export const genClearList = (value) => {
+  const clearList = {};
+
+  value.forEach(data => {
+    // eslint-disable-next-line array-callback-return
+    const _onlineList = data.versions.filter((item) => {
+      if (_.get(item, 'cluster[0].status') === 'online') return item;
+    });
+    // eslint-disable-next-line array-callback-return
+    const _offlineList = data.versions.filter((item) => {
+      if (_.get(item, 'cluster[0].status') === 'offline') return item;
+    });
+
+    if (_onlineList.length > keepOnlineNum || _offlineList.length > keepOfflineNum) {
+      clearList[data.name] = data.versions;
+    }
+  });
+
+  return clearList;
+};
 
 /**
  * 机器状态
