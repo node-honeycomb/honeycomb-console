dc37691f5eb28b1a205463647ba45deb3a33e9a2
cctv1005s <384113872@qq.com>
2021-10-27 17:44:07 +0800

    release: v2.0.4_2

--- assets/src/pages/cluster-manager/cluster-upset.jsx
+++ assets/src/pages/cluster-manager/cluster-upset.jsx
@@ -12,7 +12,7 @@ import {tryArrToLineBreakStr, removeModalDOM} from '@lib/util';
 const ipRegex = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/;
 
 // eslint-disable-next-line no-empty-character-class
-const httpIpPortRegex = new RegExp(/^(http[s]?)?:\/\/((\*)[]|((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|((\*\.)?([a-zA-Z0-9-]+\.){0,5}[a-zA-Z0-9-]+\.[a-zA-Z]{2,63}?)):[0-9]+$/g);
+const httpIpPortRegex = new RegExp(/^(http[s]?)?:\/\/(.+)/g);
 
 const layout = {
   labelCol: {span: 4},
